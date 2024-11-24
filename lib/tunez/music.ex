2fb7225f06fa17fac95e65a02d7128692bab557e
Theosaurus-Rex <theosaurus13@gmail.com>
2024-11-24 11:16:06 +1100

    feat(artist): Add create action and code interface

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -2,6 +2,8 @@ defmodule Tunez.Music do
   use Ash.Domain
 
   resources do
-    resource Tunez.Music.Artist
+    resource Tunez.Music.Artist do
+      define :create_artist, action: :create
+    end
   end
 end
